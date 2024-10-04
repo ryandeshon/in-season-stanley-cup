649d5f9b9e151e171c6c293a40c52f70c5129a58
Ryan Deshon <ryandeshon@gmail.com>
2024-10-04 16:43:47 -0400

    Add Vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,14 @@
+// src/plugins/vuetify.js
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Ensure you have Vuetify styles
+import '@mdi/font/css/materialdesignicons.css'; // Import Material Design Icons (optional)
+
+
+// Vuetify components and directives (if using Vuetify 3)
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+export default createVuetify({
+  components,
+  directives,
+});
