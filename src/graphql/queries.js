4c7ae6056aaba60d621d39c57b21281ebaf470aa
Ryan Deshon <ryandeshon@gmail.com>
2024-10-08 17:55:55 -0400

    DB and API Updates

---
+++ src/graphql/queries.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPlayer = /* GraphQL */ `
+  query GetPlayer($id: ID!) {
+    getPlayer(id: $id) {
+      id
+      name
+      teams
+      daysAsChampion
+      championships
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listPlayers = /* GraphQL */ `
+  query ListPlayers(
+    $filter: ModelPlayerFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPlayers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        teams
+        daysAsChampion
+        championships
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePlayer = /* GraphQL */ `
+  subscription OnCreatePlayer($filter: ModelSubscriptionPlayerFilterInput) {
+    onCreatePlayer(filter: $filter) {
+      id
+      name
+      teams
+      daysAsChampion
+      championships
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdatePlayer = /* GraphQL */ `
+  subscription OnUpdatePlayer($filter: ModelSubscriptionPlayerFilterInput) {
+    onUpdatePlayer(filter: $filter) {
+      id
+      name
+      teams
+      daysAsChampion
+      championships
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeletePlayer = /* GraphQL */ `
+  subscription OnDeletePlayer($filter: ModelSubscriptionPlayerFilterInput) {
+    onDeletePlayer(filter: $filter) {
+      id
+      name
+      teams
+      daysAsChampion
+      championships
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
