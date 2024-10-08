e99bec2026f7d23043690bc5a25af62a6fa3ac42
Ryan Deshon <ryandeshon@gmail.com>
2024-10-08 18:25:49 -0400

    Add keys to AWS

--- src/dynamodb-client.js
+++ src/dynamodb-client.js
@@ -1,5 +1,12 @@
 import AWS from './aws-config';
 
+// Configure AWS with the correct region
+AWS.config.update({
+  region: process.env.VUE_APP_AWS_REGION || 'us-east-1', // Replace 'us-east-1' with your region if needed
+  accessKeyId: process.env.VUE_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.VUE_APP_AWS_SECRET_ACCESS_KEY,
+});
+
 // Create a DynamoDB DocumentClient instance
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
