07ad833565aa6e2f05069b90b96b1c875d6f28e3
Ryan Deshon <ryandeshon@gmail.com>
2024-10-18 18:36:35 -0400

    Add Region

--- src/dynamodb-client.js
+++ src/dynamodb-client.js
@@ -1,6 +1,10 @@
 import AWS from './aws-config';
 
 // Create a DynamoDB DocumentClient instance
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+// Configure the AWS SDK with the region
+AWS.config.update({
+  region: process.env.VUE_APP_AWS_REGION || 'us-east-1', // Replace 'us-east-1' with your region
+});
 
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 export default dynamodb;
