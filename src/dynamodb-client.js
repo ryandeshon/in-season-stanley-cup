bcf0fc969e6236dcf2d64d1f20fb044714b35279
Ryan Deshon <ryandeshon@gmail.com>
2024-10-18 18:49:11 -0400

    Remove the duplicate AWS_REGION

--- src/dynamodb-client.js
+++ src/dynamodb-client.js
@@ -1,10 +1,9 @@
 import AWS from './aws-config';
 
-// Create a DynamoDB DocumentClient instance
 // Configure the AWS SDK with the region
 AWS.config.update({
   region: process.env.VUE_APP_AWS_REGION || 'us-east-1', // Replace 'us-east-1' with your region
 });
-
+// Create a DynamoDB DocumentClient instance
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 export default dynamodb;
