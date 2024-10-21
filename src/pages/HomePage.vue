386867c82012bcd08abba5b65bf065097452771e
Ryan Deshon <ryandeshon@gmail.com>
2024-10-20 22:30:57 -0400

    New Champ

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -41,7 +41,7 @@ export default {
   name: 'HomePage',
   data() {
     return {
-      currentChampion: 'CAR',
+      currentChampion: 'STL',
       todaysDate: DateTime.now().toFormat('yyyy-MM-dd'),
       todaysGames: [],
       todaysGame: null,
