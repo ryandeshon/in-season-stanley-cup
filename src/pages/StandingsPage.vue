63b2019e997b21daa9773961cad30ff2f08b24d9
Ryan Deshon <ryandeshon@gmail.com>
2024-11-06 18:26:49 -0500

    Add teams

--- src/pages/StandingsPage.vue
+++ src/pages/StandingsPage.vue
@@ -6,13 +6,20 @@
       <thead>
       <tr>
         <th class="text-left">Player</th>
+        <th class="text-left">Teams</th>
         <th class="text-left">Days as Champion</th>
         <th class="text-left">Title Defenses</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="standing in allPlayersData" :key="standing.name">
+      <tr v-for="standing in allPlayersData" :key="standing.name" class="py-2">
+        
         <td class="text-left">{{ standing.name }}</td>
+        <th class="flex flex-wrap justify-center align-center">
+          <div v-for="team in standing.teams" :key="team">
+            <img :src="`https://assets.nhle.com/logos/nhl/svg/${team}_light.svg`" :alt="team" class="w-6 h-6" />
+          </div>
+        </th>
         <td class="text-left">{{ standing.daysAsChampion }}</td>
         <td class="text-left">{{ standing.titleDefenses }}</td>
       </tr>
