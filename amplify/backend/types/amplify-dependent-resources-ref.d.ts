1ad539a78da1e97fa19b39867c1d2a825af0ac2c
Ryan Deshon <ryandeshon@gmail.com>
2025-07-17 12:53:38 -0400

    Reset Amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "inseasonstanleycup": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string"
-    }
-  },
-  "function": {
-    "nhlProxy": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
